e593c21146ed9c0fc30bd1afa6009e71ee9cbc8f
kapilkoushik0217 <kapilkoushik0217@gmail.com>
2023-07-06 23:42:23 +0530

    Added Routing to login and signup

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../features/navbar/Navbar";
+import ProductList from "../features/product-list/ProductList";
+const Home = () => {
+  return (
+    <div>
+      <NavBar>
+        <ProductList></ProductList>
+      </NavBar>
+    </div>
+  );
+};
+
+export default Home;
